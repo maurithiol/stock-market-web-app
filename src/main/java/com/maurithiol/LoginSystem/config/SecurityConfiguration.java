9846fa50d8a0d157e2e73424288c7733818153a0
BuildTools <unconfigured@null.spigotmc.org>
2024-02-28 09:45:54 +0100

    properties update

--- src/main/java/com/maurithiol/LoginSystem/config/SecurityConfiguration.java
+++ src/main/java/com/maurithiol/LoginSystem/config/SecurityConfiguration.java
@@ -18,7 +18,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .antMatchers("/auth/dashboard/admin/**").hasAuthority("ADMIN")
                 .antMatchers("/auth/dashboard/user/**").hasAuthority("USER")
                 .antMatchers("/**").authenticated()
-                .and().csrf().ignoringAntMatchers("/homepage") // Permet d'activer la barre de recherche
+                //.and().csrf().ignoringAntMatchers("/homepage") // Permet d'activer la barre de recherche
                 .and()
                 .formLogin()
                 .loginPage("/login")
