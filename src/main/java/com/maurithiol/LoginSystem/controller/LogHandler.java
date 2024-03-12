b222ecbc7b635e0174ae9697994858437010a453
BuildTools <unconfigured@null.spigotmc.org>
2024-03-12 20:32:17 +0100

    try out

--- src/main/java/com/maurithiol/LoginSystem/controller/LogHandler.java
+++ src/main/java/com/maurithiol/LoginSystem/controller/LogHandler.java
@@ -2,10 +2,7 @@ package com.maurithiol.LoginSystem.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import com.maurithiol.APICall.data.DataList;
@@ -74,7 +71,7 @@ public class LogHandler {
     }
 
     @GetMapping("/homepage/stock/{symbol}")
-    public String showStockData() {
+    public String showStockData(@PathVariable("symbol") String symbol, Model model) {
         return "stock";
     }
 }
